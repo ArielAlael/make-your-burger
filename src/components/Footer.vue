25ab7c6385163d64d21cd4ca407de13324213c36
Ariel <arielalael@gmail.com>
2024-02-24 11:52:56 -0300

    commit from add db, public, components and views

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer id="footer">
+    <p>Make Your Burger &copy; 2023</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+#footer {
+  height: 200px;
+  background-color: #222;
+  border-top: solid 4px #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+
+  width: 100%;
+}
+</style>
